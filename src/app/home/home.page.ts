3de26e1852c580c8b8d8941960e068786b8622d3
VictorRotvik <victorilayarodriguez@gmail.com>
2024-03-19 00:17:50 -0400

    tarea1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Componente{
+  icon:string;
+  name:string;
+  redirectTo:string;  
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  componentes: Componente[]=[
+    {
+      icon: 'american-football',
+      name: 'Action-sheet',
+      redirectTo:'/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo:'/alert'
+    },
+  ];
 
   constructor() {}
-
+  ngOnInit(){
+  }
 }
